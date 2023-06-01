46d4af3a5b1bcbd8da34e9b6a02d6565903cfeb9
xc Dou <xcdou@192.168.88.89>
2023-06-01 15:34:43 +0800

    fix: 同步VoipRoom属性

--- src/hostComponents/VoipRoom/index.ts
+++ src/hostComponents/VoipRoom/index.ts
@@ -9,6 +9,8 @@ export interface VoipRoomProps extends BaseProps {
   mode?: 'camera' | 'video';
   /** 仅在 mode 为 camera 时有效，前置或后置，值为front, back	 2.11.0 */
   devicePosition?: 'front' | 'back';
+  /** 画面与容器比例不一致时，画面的表现形式 */
+  objectFit?: 'fill' | 'contain' | 'cover';
   /** 创建对话窗口失败时触发	 2.11.0 */
   onError?: (event: GenericEvent) => any;
 }
@@ -17,8 +19,10 @@ export interface VoipRoomProps extends BaseProps {
  * 多人音视频对话。需用户授权 scope.camera、scope.record。
  * @see https://developers.weixin.qq.com/miniprogram/dev/component/voip-room.html
  */
-export const VoipRoom: React.ComponentType<VoipRoomProps> = createHostComponent<VoipRoomProps>('voip-room');
+export const VoipRoom: React.ComponentType<VoipRoomProps> =
+  createHostComponent<VoipRoomProps>('voip-room');
 VoipRoom.defaultProps = {
   mode: 'camera',
   devicePosition: 'front',
+  objectFit: 'fill',
 };
--- src/hostComponents/VoipRoom/node.ts
+++ src/hostComponents/VoipRoom/node.ts
@@ -2,6 +2,7 @@ export const alias = {
   openId: 'openid',
   mode: 'mode',
   devicePosition: 'device-position',
+  objectFit: 'object-fit',
   onError: 'binderror',
 };
 export const props = Object.values(alias);
